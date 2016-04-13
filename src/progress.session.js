31c0989ffff51a1f6a9a8b73644abee78301b3f6
whenshaw <whenshaw@progress.com>
2016-04-13 14:48:57 -0400

    Fixed authImpl error messages

--- src/progress.session.js
+++ src/progress.session.js
@@ -3427,7 +3427,7 @@ limitations under the License.
                                 "jsdoMSG033", 
                                 "JSDOSession", 
                                 "the constructor",
-                                "The authImpl property of the options parameter must have a provider object field."));                        
+                                "The authImpl property of the options parameter must have a provider object property."));                        
                     }
                     
                     // Though the consumer is optional, it needs to be an object
@@ -3438,7 +3438,7 @@ limitations under the License.
                                 "jsdoMSG033", 
                                 "JSDOSession", 
                                 "the constructor",
-                                "The authImpl property of the options parameter has an optional consumer object field."));
+                                "The authImpl.consumer property of the options parameter must be an object if it is present."));
                     }
                     
                     // TODO: Add a check if the provider has an isAuthenticated() function.
