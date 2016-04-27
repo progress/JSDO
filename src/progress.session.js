00ebbe955e3a81fff546bb02fc252a5ee0bc957e
whenshaw <whenshaw@progress.com>
2016-04-27 17:15:59 -0400

    finished implementation of SSO login

--- src/progress.session.js
+++ src/progress.session.js
@@ -2478,15 +2478,16 @@ limitations under the License.
         this._setXHRCredentials = function (xhr, verb, uri, userName, password, async) {
 
             // if using SSO, just add the token and ignore the rest of this method
-            // test for authImpl instead of a specific auth model?
             if (this.authenticationModel === progress.data.Session.AUTH_TYPE_OECP) {
-                // (deliberately letting error be thrown if any of the components of this call is missing)
                 xhr.open(verb, uri, async);
                 this.authImpl.addTokenToRequest(xhr);
-                // if we don't specify application/json, an OE Web application that's based on Form auth
-                // returns a redirect to login.jsp, which the user agent handles by getting login.jsp
-                // and returning it to our code with a status of 200. Which we could handle, but it's
-                // cleaner this way.
+
+                // We specify application/json for the response so that, if a bad token is sent, an 
+                // OE Web application that's based on Form auth will directly send back a 401.
+                // If we don't specify application/json, we'll get a redirect to login.jsp, which the
+                // user agent handles by getting login.jsp and returning it to our code with a status
+                // of 200. We could infer that authnetication failed from that, but it's much cleaner this 
+                // way.
                 xhr.setRequestHeader("Accept", "application/json");
 
                 return;
