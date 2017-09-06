a195a73a8bea1a702357981bd32f9bd13567ef31
Anil Kumar Kotha <anilkvn1241@gmail.com>
2017-09-06 15:40:17 -0400

    Updated as per review comments

--- src/progress.js
+++ src/progress.js
@@ -3006,23 +3006,22 @@ limitations under the License.
                 }
             }
 		
-			// This is the scenario where the read.call did not reach server. i.e.,
-			// some problem in between making successful call to server and we are 
-			// completing the fill() operation with necessary cleanup operations
-			if (request.success == false && request.exception) {
-				//request.deferred.reject(xhr.jsdo, request.success, request);
+            // This is the scenario where the read.call did not reach server. i.e.,
+            // some problem in between making successful call to server and we are 
+            // completing the fill() operation with necessary cleanup operations
+            if (request.success == false && request.exception) {
+                
+                if ((typeof xhr.onErrorFn) == 'function') {
+                    xhr.onErrorFn(xhr.jsdo, request.success, request);
+                }
 
-				if ((typeof xhr.onErrorFn) == 'function') {
-					xhr.onErrorFn(xhr.jsdo, request.success, request);
-				}
+                // get the Client Context ID (AppServer ID)
+                xhr.jsdo._session._checkServiceResponse(xhr, request.success, request);
 
-				// get the Client Context ID (AppServer ID)
-				xhr.jsdo._session._checkServiceResponse(xhr, request.success, request);
-
-				if ((typeof xhr.onCompleteFn) == 'function') {
-					xhr.onCompleteFn(xhr.jsdo, request.success, request);
-				}
-			}
+                if ((typeof xhr.onCompleteFn) == 'function') {
+                    xhr.onCompleteFn(xhr.jsdo, request.success, request);
+                }
+            }
 
             return promise;
         };
@@ -5845,7 +5844,7 @@ limitations under the License.
                 i;
 
             if (request && !request.success) {
-               if ((request.xhr.status >= 400 && request.xhr.status < 600) || request.xhr.status === 0) {
+               if (request.xhr.status >= 400 && request.xhr.status < 600) {
                     try {
                         responseObject = JSON.parse(request.xhr.responseText);
                         
