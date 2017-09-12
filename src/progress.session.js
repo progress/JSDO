57ce228347200284a8f11554800a17c7c9ea8ee9
Alan Estrada <joshualan23@yahoo.com>
2017-09-12 18:07:26 -0400

    Added invalidateAllSessions and ServMan cleanup.

--- src/progress.session.js
+++ src/progress.session.js
@@ -1,5 +1,5 @@
 /* 
-progress.session.js    Version: 4.4.0-11
+progress.session.js    Version: 4.4.1-1
 
 Copyright (c) 2012-2017 Progress Software Corporation and/or its subsidiaries or affiliates.
  
@@ -33,6 +33,7 @@ limitations under the License.
     progress.data.ServicesManager._resources = [];
     progress.data.ServicesManager._data = [];
     progress.data.ServicesManager._sessions = [];
+    progress.data.ServicesManager._jsdosessions = [];    
     /*
      progress.data.ServicesManager.put = function(id, jsdo) {
      progress.data.ServicesManager._data[id] = jsdo;
@@ -66,12 +67,63 @@ limitations under the License.
         else
             throw new Error("Cannot load catalog '" + catalogURI + "' multiple times.");
     };
+
+    progress.data.ServicesManager.addJSDOSession = function (catalogURI, jsdosession) {
+        if (progress.data.ServicesManager._jsdosessions[catalogURI] === undefined) {
+            progress.data.ServicesManager._jsdosessions[catalogURI] = jsdosession;
+        }
+        else {
+            throw new Error("Cannot load catalog '" + catalogURI + "' multiple times.");
+        }
+    };
     progress.data.ServicesManager.getSession = function (catalogURI) {
         try {
             return progress.data.ServicesManager._sessions[catalogURI];
         }
         catch (e) {
             return null;
+        }
+    };
+
+    progress.data.ServicesManager.cleanSession = function (session) {
+        var servicesKey,
+        resourcesKey,
+        sessionsKey,
+        service,
+        services = progress.data.ServicesManager._services,
+        resources = progress.data.ServicesManager._resources,
+        sessions = progress.data.ServicesManager._sessions,
+        jsdosessions = progress.data.ServicesManager._jsdosessions;
+        
+        // Delete the services and resources in the ServicesManager
+        // associated with the Session given
+        for (servicesKey in services) {
+            service = null;
+            if (services[servicesKey]._session === session) {
+                service = services[servicesKey];
+                delete services[servicesKey];                    
+            }
+
+            if (!service) {
+                continue;
+            }
+
+            for (resourcesKey in resources) {
+                if (resources[resourcesKey].service === service) {
+                    delete resources[resourcesKey];
+                }
+            }
+        }
+
+        // Delete the session and jsdosession from the ServicesManager
+        for (sessionsKey in sessions) {
+            if (sessions[sessionsKey] === session) {
+                delete sessions[sessionsKey];
+
+                if(jsdosessions[sessionsKey]) {
+                    delete jsdosessions[sessionsKey];
+                }
+            }
         }
     };
 
@@ -2093,6 +2145,7 @@ limitations under the License.
             setUserName(null, pdsession);
             _password = null;
             setAuthProvider(null);
+            cleanServicesManager();            
 
 
             if (success) {
@@ -2317,7 +2370,8 @@ limitations under the License.
             var theSession = xhr.pdsession;
             var servicedata;
             var catalogURI = xhr._catalogURI,
-                serviceURL;
+                serviceURL,
+                theJSDOSession = jsdosession;
 
             // Only change the Session's state if the default AuthProv is being used
             if (!customCredentials) {
@@ -2360,6 +2414,9 @@ limitations under the License.
                 }
                 pushCatalogURIs(catalogURI, theSession);
                 progress.data.ServicesManager.addSession(catalogURI, theSession);
+                if (theJSDOSession) {
+                    progress.data.ServicesManager.addJSDOSession(catalogURI, theJSDOSession);                
+                }
             }
             else if (_catalogHttpStatus == 401) {
                 return progress.data.AuthenticationProvider._getAuthFailureReason(xhr);
@@ -3152,6 +3209,11 @@ limitations under the License.
             }
         }
 
+        // Remove all resources, services, and sessions related to this Session from the ServicesManager
+        function cleanServicesManager() {
+            progress.data.ServicesManager.cleanSession(that);            
+        }
+
         // process constructor options and do other initialization
         
         // If a storage key (name property of a JSDOSession) was passed to the constructor, 
@@ -3927,7 +3989,11 @@ limitations under the License.
             
             this.disconnect()
                 .then(function () {
-                    return authProv.logout();
+                    if (authProv) {
+                        return authProv.logout();
+                    }
+                    // if there's no AP, just resolve immediately
+                    deferred.resolve(that, result, info);   
                 })
                 .then(function (jsdosession, result, info) {
                     deferred.resolve(that, result, info);
@@ -4399,6 +4465,32 @@ limitations under the License.
         
         return deferred.promise();
     };
+
+    progress.data.invalidateAllSessions = function () {
+        var jsdosession,
+            key,
+            deferred = $.Deferred(),
+            jsdosessions = progress.data.ServicesManager._jsdosessions,
+            logoutPromises = [];
+
+        for (key in jsdosessions) {
+            if (jsdosessions.hasOwnProperty(key)) {
+                jsdosession = jsdosessions[key];
+
+                logoutPromises.push(jsdosession.logout());
+            }
+        }
+
+        $.when.apply($, logoutPromises)
+            .then(function () {
+                deferred.resolve(progress.data.Session.SUCCESS);                
+            }, function (session, result, info) {
+                deferred.reject(progress.data.Session.GENERAL_FAILURE, info);
+            });
+
+        // Using beautiful jquery shenanigans
+        return deferred.promise();
+    }; 
 
 })();
 
