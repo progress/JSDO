fe67268c43efdae97689ab28426ef279511ae4c1
edselg <egarcia@progress.com>
2018-04-12 12:46:15 -0400

    Add loaddep.js files for NS and Node.js

--- src/progress.util.js
+++ src/progress.util.js
@@ -20,7 +20,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
  */
-/*global progress:true, btoa:true*/
+/*global progress:true*/
 /*jslint nomen: true*/
 
 (function () {
