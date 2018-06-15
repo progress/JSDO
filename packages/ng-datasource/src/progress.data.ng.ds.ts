60fdc76a9579d9f4e6c0c4392532374c78d8b1c1
edselg <egarcia@progress.com>
2018-06-15 17:43:48 -0400

    Changed info.batch to request.batch

--- packages/ng-datasource/src/progress.data.ng.ds.ts
+++ packages/ng-datasource/src/progress.data.ng.ds.ts
@@ -575,8 +575,8 @@ export class DataSource {
                             resolve(responseData);
                         } else {
                             // Non-Submit case
-                            if (result.info.batch.operations && result.info.batch.operations.length > 0) {
-                                result.info.batch.operations.forEach((operation) => {
+                            if (result.request && result.request.batch.operations && result.request.batch.operations.length > 0) {
+                                result.request.batch.operations.forEach((operation) => {
                                     this._copyRecord(operation.response, responseData);
                                     // In case of multiple operations we want to merge those records pertaining
                                     // to different operations in a single dataset and is sent as part of the
@@ -586,9 +586,9 @@ export class DataSource {
                                 resolve(promResponse);
                                 // Scenario where the saveChanges is invoked directly without any Submit/Non-Submit
                                 // service as the serviceURI. We will resolve with an empty object
-                            } else if (result.info.batch.operations.length === 0) {
+                            } else if (result.request && result.request.batch.operations.length === 0) {
                                 resolve({});
-                            } else { // Reject promise if either of above cases are met
+                            } else { // Reject promise if either of above cases are not met
                                 reject(new Error(this
                                     .normalizeError(result, "saveChanges", "Errors occurred while saving Changes.")));
                             }
