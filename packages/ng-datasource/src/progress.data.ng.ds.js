f47329666c96701a75dd29ee844624662e478dfe
Anil Kumar Kotha <anilkumar.audacious@gmail.com>
2018-09-19 17:53:25 -0400

    Updated JavaScript code to include decorate statements

--- packages/ng-datasource/src/progress.data.ng.ds.js
+++ packages/ng-datasource/src/progress.data.ng.ds.js
@@ -24,6 +24,13 @@ between OpenEdge (Progress Data Object) with NativeScript.
 Author(s): maura, anikumar, egarcia
 
 */
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var jsdo_core_1 = require("@progress/jsdo-core");
