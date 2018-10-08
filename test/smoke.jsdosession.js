34629b906ee3846bf763caf5d4f47d95c0e22a11
Alan Estrada <aestrada@progress.com>
2018-10-08 11:02:46 -0400

    Changed the basic credentials

--- test/smoke.jsdosession.js
+++ test/smoke.jsdosession.js
@@ -38,11 +38,11 @@ describe('JSDOSession Smoke Tests', () => {
 
         it('should connect to an existing basic backend', function () {
             let getSession = progress.data.getSession({
-                serviceURI: 'http://oemobiledemo.progress.com/OEMobileDemoServicesBasic/',
-                catalogURI: 'http://oemobiledemo.progress.com/OEMobileDemoServicesBasic/static/CustomerService.json',
+                serviceURI: 'http://172.29.18.125:8894/OEMobileDemoServicesBasic',
+                catalogURI: 'http://172.29.18.125:8894/OEMobileDemoServicesBasic/static/CustomerService.json',
                 authenticationModel: 'basic',
-                username: 'basicuser',
-                password: 'basicpassword'
+                username: 'restuser',
+                password: 'password'
             }).then((object) => {
                 object.jsdosession.invalidate();
                 return object.result; 
